299bbe16b59593d4cbc8201b719ab57aa85097b4
Josué <josue@informaticos.com>
2018-09-18 08:16:43 +0200

    First commit

---
+++ Composers/CurrentUserViewComposer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Modules\Core\Composers;
+
+use Illuminate\Contracts\View\View;
+use Modules\User\Contracts\Authentication;
+
+class CurrentUserViewComposer
+{
+    /**
+     * @var Authentication
+     */
+    private $auth;
+
+    public function __construct(Authentication $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    public function compose(View $view)
+    {
+        $view->with('currentUser', $this->auth->user())
+             /* GOBALO MOD */
+             ->with('defaultLocale' , env('LOCALE'));
+             /* FIN GOBALO MOD */
+    }
+}
